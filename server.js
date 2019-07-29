6c43c5c6bc48ac060339969dff2a3c3753fc2663
rheamac <rheamac999@gmail.com>
2019-07-28 21:02:25 -0400

    added funcionality for post and retreive messages

--- server.js
+++ server.js
@@ -16,18 +16,24 @@ const dbName = 'messageBoard';
 let db;
 
 
-
-app.post('/api/message',(req, res) => {
-    console.log(req.body);
+// post data from client to database with status code
+app.post('/api/message', (req, res) => {
     db.collection('messages').insertOne({'msg': req.body});
     res.status(200).send();
+});
+
+// to get all messages
+app.get('/api/message', async (req, res) => {
+  const docs = await db.collection('messages').find({}).toArray();
+  if(!docs) return res.json({error:'Error fetching messages'});
+  res.json(docs);
 });
 
 // Use connect method to connect to the Server
 MongoClient.connect(url,function(err, client) {
     if(err) return console.log("mongodb error", err);
-    console.log("Connected successfully to server"); 
     db =client.db(dbName);
   });
 
+  // to listen to the port
 app.listen(port, ()=> console.log("app is running", port));
